cb28633de6bfe3e29716a262732190a420b93a91
ellereyy <elle.m.reynolds@gmail.com>
2023-04-20 22:46:35 -0700

    vite frontend structure started

---
+++ mern-app/src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react'
+
+import Button from '../ui/button.jsx'
+import NavBar from '../ui/NavBar.jsx'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <p>test test</p>
+      <Button />
+    </>
+  )
+}
+
+export default App
